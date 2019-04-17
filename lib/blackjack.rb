7c6a357cc88cdfb393bd3db9d167f64b21d55de8
Adrian Samaniego <llathander@gmail.com>
2019-04-17 00:33:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,8 +27,8 @@ end
 def initial_round
   first_card = deal_card
   second_card = deal_card
-  total = first_card + second_card
-  display_card_total(total)
+  card_total = first_card + second_card
+  display_card_total(card_total)
   
   # code #initial_round here
 end

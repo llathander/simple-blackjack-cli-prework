1a2f35508dff27206467d78c027a7a80de045874
Adrian Samaniego <llathander@gmail.com>
2019-04-17 00:54:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,6 +36,12 @@ end
 
 def hit?
   # code hit? here
+    prompt_user 
+  get_user_input = 
+  if get_user_input == "h"
+    card_total += 
+  elsif get_user_input == "s"
+    card_total 
 end
 
 def invalid_command
